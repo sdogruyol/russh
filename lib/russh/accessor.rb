56dc332b5ca14aac89607c67fad39639b5e0a898
Serdar Dogruyol <dogruyolserdar@gmail.com>
2014-08-09 12:24:38 +0300

    Added existing check and format to accessor

--- lib/russh/accessor.rb
+++ lib/russh/accessor.rb
@@ -6,18 +6,40 @@ module Russh
 
     def initialize
       @path = Dir.home + '/.ssh/config'
-      @is_readable = File.readable? @path
-      @is_writable = File.writable? @path
+      @is_existing = is_existing?
+      @is_readable = is_readable?
+      @is_writable = is_writable?
     end
 
+    # Checks if the config file is existing. If not it creates a new one.
+    def is_existing?
+      if File.exist? @path
+        @is_existing = true
+      else
+        puts "You don't have an existing ssh config file. Creating a new one for you."
+        File.new(@path, 'w')
+      end
+    end
+
+    # Checks if the config file is readable.
     def is_readable?
-      @is_readable
+      begin
+        @is_readable = File.readable? @path
+      rescue => e
+        puts "Couldn't be able to read your ssh config: Error #{e}"
+      end
     end
 
+    # Checks if the config file is writable.
     def is_writable?
-      @is_writable
+      begin
+        @is_writable = File.writable? @path
+      rescue => e
+        puts "Couldn't be able to write your ssh config: Error #{e}"
+      end
     end
 
+    # Backups the config file to config.bk
     def backup
       if is_readable?
         FileUtils.cp @path, "#{@path}.bk"
@@ -31,17 +53,27 @@ module Russh
     end
 
     def create
+      # Ask the user for input
       @host = ask 'Host'
       @host_name = ask 'HostName'
       @user = ask 'User'
+      # Check for read and write access
       if is_readable? && is_writable?
         backup
-        open(@path, 'a') do |f|
-          f.puts "Host #{@host}"
-          f.puts "  HostName #{@host_name}"
-          f.puts "  User #{@user}"
+        open(@path, 'a+') do |f|
+          # If the file is new don't add a newline.
+          f.puts if f.readlines.size > 0
+          # Format each entry
+          f.puts format_entry
         end
       end
+    end
+
+    protected
+
+    # Formats each entry
+    def format_entry
+      "Host #{@host}\n\tHostName #{@host_name}\n\tUser #{@user}"
     end
   end
 end
